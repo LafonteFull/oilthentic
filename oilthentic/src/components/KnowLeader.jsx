47f1a62c405c8ab1bea5a6e2874a750b1f510b2c
Rivan-byte02 <visquare@Visquares-MacBook-Pro.local>
2021-03-14 09:15:10 +0700

    done create function sign-up

--- oilthentic/src/components/KnowLeader.jsx
+++ oilthentic/src/components/KnowLeader.jsx
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 const KnowLeader = () => {
+  const [userInput, setUserInput] = useState()
+  
+
+  const onChange = (e) => {
+    let { name, value } = e.target;
+    const newInput = { ...userInput, [name]: value };
+    setUserInput(newInput);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log(userInput);
+  }
 
   return (
     <div className='md:w-1/3 lg:w-1/3 w-full justify-center'>
       <div className="my-10 mx-auto p-5 rounded-md shadow-sm border-solid border border-gray-200">
-        <form className="flex justify-center flex-row ">
+        <form
+        onSubmit={onSubmit}
+        className="flex justify-center flex-row ">
           <div className="divide-y divide-gray-200 w-full px-5">
             <div className="py-8 text-base text-gray-700">
               <div className="flex-1 flex-col">
                 <label className="leading-loose">Name</label>
                 <input
-                  name="Name"
+                  name="name"
                   type="text"
                   className="block w-full py-3 px-1 mt-2 
                   text-gray-800 appearance-none 
                   border-b-2 border-gray-100
                   focus:text-gray-500 focus:outline-none focus:border-gray-200"
                   placeholder="e.g  John Doe"
-                  // onChange={onChange}
+                  onChange={onChange}
                 />
               </div>
               <div className="flex-1 flex-col my-3">
@@ -31,7 +46,7 @@ const KnowLeader = () => {
                   border-b-2 border-gray-100
                   focus:text-gray-500 focus:outline-none focus:border-gray-200"
                   placeholder="e.g  johndoe@mail.com"
-                  // onChange={onChange}
+                  onChange={onChange}
                 />
               </div>
               <div className="flex-1 flex-col ">
@@ -44,12 +59,16 @@ const KnowLeader = () => {
                   border-b-2 border-gray-100
                   focus:text-gray-500 focus:outline-none focus:border-gray-200"
                   placeholder="e.g +6281x-xxxx-xxxx"
-                  // onChange={onChange}
+                  onChange={onChange}
                 />
               </div>
               <div className="flex-1 flex-col mt-3">
                 <label className="leading-loose">Location</label>
-                <select id="location" className="mt-2 px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600">
+                <select
+                name='location'
+                onChange={onChange}
+                id="location" className="mt-2 px-4 py-2 border focus:ring-gray-500 focus:border-gray-900
+                w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600">
                   <option value="jakarta">Jakarta</option>
                   <option value="bandung">Bandung</option>
                   <option value="yogyakarta">Yogyakarta</option>
