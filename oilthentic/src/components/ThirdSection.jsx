7dd4caefc50cff558a019a54bda1d156cc3fc7d0
Amanda Rizqi <amanda.rzq@gmail.com>
2021-03-28 16:56:28 +0700

    font resizing

--- oilthentic/src/components/ThirdSection.jsx
+++ oilthentic/src/components/ThirdSection.jsx
@@ -9,7 +9,7 @@ const ThirdSection = (props) => {
             <div className="flex flex-col items-center px-2 md:mt-5 w-full">
               <h3 className="purple-text merry-font text-center font-semibold md:text-3xl xl:text-4xl"
               style={{ lineHeight: '1.5' }}>Promo yang Bisa Kamu Dapatkan di <br/> Oilthentic Day 2021</h3>
-              <p className="poppins-font purple-text text-justify text-xs mt-3 md:text-3xl md:mt-20 md:px-10 lg:px-20"> Hadiah didapatkan untuk setiap pembelian PEP Young Living Indonesia di tanggal 25 dan 26 April 2021 berdasarkan total jumlah share yang dihasilkan.</p>
+              <p className="poppins-font purple-text text-justify text-xs mt-3 md:text-2xl md:mt-20 md:px-10 lg:px-20"> Hadiah didapatkan untuk setiap pembelian PEP Young Living Indonesia di tanggal 25 dan 26 April 2021 berdasarkan total jumlah share yang dihasilkan.</p>
               <div className="flex flex-row justify-center items-center md:px-10 py-2 mt-2 w-full ">
                 <img src={bothproducts} alt="Products" className="w-full lg:w-3/4 xl:w-2/4" style={{ minWidth: '40vw' }} 
                 //  style={{ width: props.isMobile ? '90vw' : '100rem' }}  
